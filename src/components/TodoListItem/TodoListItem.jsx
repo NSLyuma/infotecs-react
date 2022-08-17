f28c97bf8eb7f76d934c08e2536e8eb8961d1e87
NSLyuma <iinokayo@mail.ru>
2022-08-17 22:22:35 +0300

    Added tasks shortening

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,7 +1,9 @@
+import styles from "./TodoListItem.module.css";
+
 export const TodoListItem = ({ task, id, editTask, deleteTask }) => {
   return (
     <li>
-      <div>{task}</div>
+      <p className={styles.task}>{task}</p>
       <button onClick={() => editTask(id, task)}>Редактировать</button>
       <button onClick={() => deleteTask(id)}>Удалить</button>
     </li>
